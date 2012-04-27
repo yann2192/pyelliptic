9d3071ec8c1b9dd5e30fbcb090284130f1be4769
Yann GUIBET <yannguibet@gmail.com>
2012-04-27 07:20:29 -0500

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,11 @@
-import os
-import re
-import codecs
 from setuptools import setup, find_packages
-
-def read(*parts):
-    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
 setup(
     name="pyelliptic",
-    version=find_version("pyelliptic", "__init__.py"),
+    version='1.0',
     url='https://github.com/yann2192/pyelliptic',
     license='GPL',
     description="Python OpenSSL wrapper. For modern cryptography with ECC, AES, HMAC, Blowfish, ...",
-    long_description=read('README'),
     author='Yann GUIBET',
     author_email='yannguibet@gmail.com',
     packages=find_packages(),
