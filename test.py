daa166e3c2122bca6273a7da981770d952fab28a
Dionysis Zindros <dionyziz@gmail.com>
2014-08-17 20:42:06 +0300

    Shorten lines in test.py according to pep8

--- test.py
+++ test.py
@@ -35,9 +35,13 @@ from binascii import hexlify, unhexlify
 
 print("TEST: AES-256-CTR")
 ciphername = "aes-256-ctr"
-iv = unhexlify(b"f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff")
-key = unhexlify(b"603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4")
-plaintext = unhexlify(b"6bc1bee22e409f96e93d7e117393172a")
+
+iv_hex = b"f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"
+iv = unhexlify(iv_hex)
+key_hex = b"603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
+key = unhexlify(key_hex)
+plain_hex = b"6bc1bee22e409f96e93d7e117393172a"
+plaintext = unhexlify(plain_hex)
 
 ctx = Cipher(key, iv, 1, ciphername=ciphername)
 enc = ctx.ciphering(plaintext)
@@ -49,9 +53,12 @@ assert ctx.ciphering(enc) == plaintext
 
 print("\nTEST: AES-256-CFB")
 ciphername = "aes-256-cfb"
-key = unhexlify(b"603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4")
-iv = unhexlify(b"000102030405060708090A0B0C0D0E0F")
-plaintext = unhexlify(b"6bc1bee22e409f96e93d7e117393172a")
+key_hex = b"603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
+key = unhexlify(key_hex)
+iv_hex = b"000102030405060708090A0B0C0D0E0F"
+iv = unhexlify(iv_hex)
+plain_hex = b"6bc1bee22e409f96e93d7e117393172a"
+plaintext = unhexlify(plain_hex)
 
 ctx = Cipher(key, iv, 1, ciphername=ciphername)
 enc = ctx.ciphering(plaintext)
@@ -63,9 +70,12 @@ assert ctx.ciphering(enc) == plaintext
 
 print("\nTEST: AES-256-CBC")
 ciphername = "aes-256-cbc"
-iv = unhexlify(b"000102030405060708090A0B0C0D0E0F")
-key = unhexlify(b"603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4")
-plaintext = unhexlify(b"6bc1bee22e409f96e93d7e117393172a")
+iv_hex = b"000102030405060708090A0B0C0D0E0F"
+iv = unhexlify(iv_hex)
+key_hex = b"603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
+key = unhexlify(key_hex)
+plain_hex = b"6bc1bee22e409f96e93d7e117393172a"
+plaintext = unhexlify(plain_hex)
 
 ctx = Cipher(key, iv, 1, ciphername=ciphername)
 enc = ctx.ciphering(plaintext)
