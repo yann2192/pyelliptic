e028c8118ce56f29785d99dd7e1f104aa736a468
Yann GUIBET <yannguibet@gmail.com>
2014-10-09 13:52:12 +0200

    update CHANGES.txt and version

--- pyelliptic/__init__.py
+++ pyelliptic/__init__.py
@@ -31,7 +31,7 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '1.5.4'
+__version__ = '1.5.5'
 
 __all__ = [
     'OpenSSL',
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyelliptic",
-    version='1.5.4',
+    version='1.5.5',
     url='https://github.com/yann2192/pyelliptic',
     license='GPL',
     description=
